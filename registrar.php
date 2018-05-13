4d88b4834fa99295b1ec3c7aca97933aed29430b
unknown <luis.fernandoch24@gmail.com>
2018-05-13 03:47:31 -0700

    Mandar datos de registro a BD

--- registrar.php
+++ registrar.php
@@ -1,5 +1,32 @@
 <?php
 
+//Asignar variable de conexion
+ $conexion = mysqli_connect("localhost","root","","condesa");
+
+ //Asignar datos del HTML en variables
+ $nombre = $_POST["nombre"];
+ $apellido = $_POST["apellido"];
+ $email = $_POST["email"];
+ $telefono = $_POST["telefono"];
+ $password = $_POST["password"];
+// $password2 = $_POST["password2"];
+
+ //Consulta
+ $insertar = "INSERT INTO login_clientes(nombre, apellido, password, email, telefono) VALUES('$nombre', 
+ '$apellido', '$password', '$email', '$telefono')";
+
+ //Ejecutar consulta
+ $resultado = mysqli_query($conexion, $insertar);
+
+ if(!$resultado){
+     echo'Error';
+ }else{
+     echo'OK jeje';
+ }
+
+ //cerrar conexion
+ mysqli_close($conexion);
+
 
 
 ?>
