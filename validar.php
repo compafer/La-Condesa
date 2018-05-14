9566cb0717eccd1d79b4040c53035d4ec2c982f1
unknown <luis.fernandoch24@gmail.com>
2018-05-13 18:16:55 -0700

    Sesion

--- validar.php
+++ validar.php
@@ -1,9 +1,13 @@
 <?php
+
 $email=$_POST['email'];
 $password=$_POST['password'];
-
+session_start();
 //Conexion a base de datos
 $conexion=mysqli_connect("localhost","root","","condesa");
+//$nombre =" SELECT nombre, apellido FROM login_clientes WHERE email='$email'";
+
+$_SESSION['email'] = $email;
 
 $consulta="SELECT * FROM login_clientes WHERE email='$email' and password='$password'";
 
@@ -11,9 +15,9 @@ $resultado=mysqli_query($conexion,$consulta);
 
 $filas=mysqli_num_rows($resultado);
 
-//Si es correcto te manda a "pedido.html" y si no, escribe "Datos Invalidos"
+//Si es correcto te manda a "pedido.php" y si no, escribe "Datos Invalidos"
 if ($filas>0){
-    header("location:pedido.html");
+    header("location:pedido.php");
 }
 else{
     echo "Datos Invalidos";
